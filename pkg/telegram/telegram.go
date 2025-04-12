5032caeec14d81b4675461745566400e22b561fb
Egor <klin0312@gmail.com>
2025-04-12 17:54:28 +0300

    add: demarshal in telegram func

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -1,8 +1,9 @@
 package telegram
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 
@@ -11,9 +12,14 @@ import (
 
 var Bot *tgbotapi.BotAPI
 
+type Metrics struct {
+	CPUUsage float64 `json:"cpu_usage"`
+	RAMUsage float64 `json:"ram_usage"`
+}
+
 func StartBot() {
 	var err error
-	Bot, err = tgbotapi.NewBotAPI("7770574747:AAHbM83zkXo4l407UdAcilXTlkxIaWEHvwc")
+	Bot, err = tgbotapi.NewBotAPI("")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -36,8 +42,15 @@ func StartBot() {
 }
 
 func handleMessage(message *tgbotapi.Message) {
-	if message.Text == "/status" {
-		resp, err := http.Get("http://localhost:8080/status")
+	if message.Text == "/start" {
+		// Отправка клавиатуры
+		sendKeyboard(message.Chat.ID)
+	}
+
+	// /status
+	if message.Text == "Получить статус системы" {
+		// Данные с API
+		resp, err := http.Get("http://89.104.70.198:8080/status")
 		if err != nil {
 			log.Println("Ошибка получения данных с API:", err)
 			sendMessage(message.Chat.ID, "Не удалось получить данные о метриках.")
@@ -45,21 +58,52 @@ func handleMessage(message *tgbotapi.Message) {
 		}
 		defer resp.Body.Close()
 
-		body, err := ioutil.ReadAll(resp.Body)
+		// Чтение и демаршализация JSON
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Println("Ошибка чтения ответа API:", err)
 			sendMessage(message.Chat.ID, "Ошибка при обработке данных.")
 			return
 		}
 
-		sendMessage(message.Chat.ID, fmt.Sprintf("⚡️ Состояние системы:\n\n%s", string(body)))
+		var metrics Metrics
+		err = json.Unmarshal(body, &metrics)
+		if err != nil {
+			log.Println("Ошибка декодирования JSON:", err)
+			sendMessage(message.Chat.ID, "Ошибка при обработке данных.")
+			return
+		}
+
+		// Формирование ответа для пользователя
+		messageText := fmt.Sprintf("⚡️ Состояние системы:\n\nCPU: %.2f%%\nRAM: %.2f%%", metrics.CPUUsage, metrics.RAMUsage)
+		sendMessage(message.Chat.ID, messageText)
 	}
 }
 
+// Функция для отправки сообщений в Telegram
 func sendMessage(chatID int64, text string) {
 	msg := tgbotapi.NewMessage(chatID, text)
 	_, err := Bot.Send(msg)
 	if err != nil {
 		log.Println("Ошибка отправки сообщения в Telegram:", err)
+	}
+}
+
+// Функция для отправки клавиатуры
+func sendKeyboard(chatID int64) {
+	// Создание кнопки на клавиатуре
+	button := tgbotapi.NewKeyboardButton("Получить статус системы")
+
+	// Создание клавиатуры
+	keyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(button),
+	)
+
+	// Отправка клавиатуры
+	msg := tgbotapi.NewMessage(chatID, "Привет! Я сервер Егора! Выбери опцию, которая есть ниже в клвиатуре!")
+	msg.ReplyMarkup = keyboard
+	_, err := Bot.Send(msg)
+	if err != nil {
+		log.Println("Ошибка отправки клавиатуры в Telegram:", err)
 	}
 }
